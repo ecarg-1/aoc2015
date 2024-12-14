6775250a77357b7a518a4f7ba9a1e068caff84dd
ecarg-1 <graceboltz15@gmail.com>
2024-12-13 21:57:26 -0500

    Update Day6.py

--- Day-6/Day6.py
+++ Day-6/Day6.py
@@ -26,12 +26,18 @@ print(int(sum(lights.flatten())))
 #Part 2
 def do_something_else(mat, start, stop, direction):
     match direction:
-        case 'on':
-            pass
-        case 'off':
-            pass
+        case 'on': #inc by 1
+            temp = mat[start[0]:stop[0]+1, start[1]:stop[1]+1]+1
+            mat[start[0]:stop[0]+1,start[1]:stop[1]+1] = temp
+        case 'off': 
+            temp = mat[start[0]:stop[0]+1, start[1]:stop[1]+1]-1
+            temp = np.where(temp < 0, 0, temp)
+            mat[start[0]:stop[0]+1,start[1]:stop[1]+1] = temp
+            
         case 'toggle':
-            pass
+            temp = mat[start[0]:stop[0]+1, start[1]:stop[1]+1]+2
+            mat[start[0]:stop[0]+1,start[1]:stop[1]+1] = temp
 brights = np.zeros([1000,1000])
 for dir in directions:
-    pass
+    do_something_else(brights, [int(dir[1]), int(dir[2])], [int(dir[3]), int(dir[4])], dir[0])
+print(int(sum(brights.flatten())))
